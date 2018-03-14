9635554461778e9e63f5691ffea6ce852b7495e9
huangsenda <huang.senda@qianka.com>
2018-03-14 11:04:17 +0800

    详情页

--- pages/details/_id.vue
+++ pages/details/_id.vue
@@ -1,33 +1,263 @@
 <template>
   <div class="container">
-    <div @click="back('/')">返回首页</div>
-    <div>当前jobId为： {{ jobId }}</div>
+    <DetailHeader
+      title="兼聘兼职"
+      :hasSearch="true"
+      :on-back-button-click="back"
+    ></DetailHeader>
+    <div class="detail-title">
+      <p class="title">苹果手机APP试玩在家随时可做</p>
+      <p class="salary"><span class="num">50</span>元/小时</p>
+      <div class="label">
+        <div class="label-item">日结</div>
+        <div class="label-item">9999人</div>
+        <div class="clear"></div>
+      </div>
+      <span><img class="hot" src="//assets.qkcdn.com/images/302d958266ec83b5f0762e3f443400d6.png"></span>
+    </div>
+
+    <div class="detail-detail">
+      <div class="title">职位描述</div>
+      <div class="description">资邸平台提供高净值客户针对优质抵押物进行投资的信息撮合服务，主打一线城市住宅别墅，提供一系列完善的解决方案，所有项目均通过国家房地产交易中心办理抵押登记，以此确保借款人房产抵押在出借人本人名下，保证本金利息权益，实现财富稳定增长。</div>
+      <div class="btn-more" @click="moreDetail"><span v-text="is_more?'收起': '展开'"></span><img class="btn-more-img" :src="is_more?'//assets.qkcdn.com/images/396f6c3c0fd033f6be32cc660001d007.png': '//assets.qkcdn.com/images/949066b0ae6761f707d74a6c45b0e014.png'"></div>
+      <div class="clear"></div>
+      <div class="income">
+        <div class="title">薪资福利</div>
+        <div class="content">每天结算，50/小时</div>
+      </div>
+    </div>
+
+    <div class="detail-more" v-if="is_more">
+      <div class="detail-time">
+        <div class="title">工作时间</div>
+        <div class="content">不限时间地点，随时可做</div>
+      </div>
+      <div class="detail-add">
+        <div class="title">工作地点</div>
+        <div class="content">全国－不限</div>
+      </div>
+    </div>
+
+    <div class="detail-more detail-co" v-if="is_more">
+      <span class="name-title">公司名称</span>
+      <span class="name">方龙</span>
+    </div>
+
+    <div class="btn-commit">我要报名</div>
   </div>
 </template>
 <style lang="scss" scoped>
   .container {
-    margin-top: 50px;
-    text-align: center;
-    height: 200px;
+    min-height: 100vh;
+    background: #f5f5f5;
+
+    .clear {
+      clear: both;
+    }
+
+    .detail-title {
+      position: relative;
+      padding: 18px 14px 20px 16px;
+      background: #FFFFFF;
+      margin-bottom: 10px;
+
+      .title {
+        font-family: PingFangSC-Semibold;
+        font-size: 18px;
+        color: #444749;
+        width: 100%;
+        margin-bottom: 17px;
+        display: inline-block;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+
+      .salary {
+        font-family: PingFangSC-Regular;
+        font-size: 14px;
+        color: #6ED9CA;
+        margin-bottom: 8px;
+
+        .num {
+          font-family: DINMittelschriftStd;
+          font-size: 26px;
+        }
+      }
+
+      .label {
+        font-family: PingFangSC-Regular;
+        font-size: 12px;
+        color: #444749;
+
+        .label-item {
+          float: left;
+          height: 8px;
+          line-height: 8px;
+          border-right: solid 1px #B5B5B5;
+          padding-left: 5px;
+          padding-right: 5px;
+
+          &:nth-child(1) {
+            padding-left: 0;
+          }
+          &:nth-last-child(2) {
+            border-right: none;
+          }
+        }
+      }
+
+      .hot {
+        position: absolute;
+        width: 40px;
+        height: 39px;
+        right: 19px;
+        bottom: 21px;
+      }
+    }
+
+    .detail-detail {
+      position: relative;
+      padding: 20px 16px 0 20px;
+      background: #FFFFFF;
+
+      .title {
+        font-family: PingFangSC-Semibold;
+        font-size: 16px;
+        color: #444749;
+        margin-bottom: 10px;
+      }
+
+      .description {
+        position: relative;
+        font-family: PingFangSC-Regular;
+        font-size: 13px;
+        color: #B5B5B5;
+        margin-bottom: 14px;
+      }
+
+      .btn-more {
+        position: relative;
+        font-family: PingFangSC-Regular;
+        font-size: 12px;
+        color: #444749;
+        text-align: center;
+        line-height: 12px;
+        display: inline-block;
+        float: right;
+        margin-bottom: 14px;
+
+        span {
+          margin-right: 6px;
+        }
+
+        .btn-more-img {
+          width: 12px;
+        }
+      }
+
+      .income {
+        position: relative;
+        padding: 17px 0 24px;
+        border-top: solid 1px #eeeeee;
+
+        .content {
+          font-family: PingFangSC-Regular;
+          font-size: 13px;
+          color: #B5B5B5;
+        }
+      }
+    }
+
+    .detail-more {
+      position: relative;
+      padding: 0 20px;
+      background: #fff;
+      margin-top: 10px;
+
+      .title {
+        font-family: PingFangSC-Semibold;
+        font-size: 16px;
+        color: #444749;
+        margin-bottom: 10px;
+      }
+
+      .content {
+        font-family: PingFangSC-Regular;
+        font-size: 13px;
+        color: #B5B5B5;
+      }
+
+      .detail-time {
+        position: relative;
+        padding: 17px 0 24px;
+        border-bottom: solid 1px #eeeeee;
+      }
+
+      .detail-add {
+        position: relative;
+        padding: 17px 0 24px;
+      }
+
+      .name-title {
+        font-family: PingFangSC-Regular;
+        font-size: 13px;
+        color: #444749;
+        margin-right: 10px;
+      }
+
+      .name {
+        font-family: PingFangSC-Regular;
+        font-size: 13px;
+        color: #B5B5B5;
+      }
+    }
+
+    .detail-co {
+      padding: 10px 20px;
+      margin-bottom: 15px;
+    }
+
+    .btn-commit {
+      position: relative;
+      width: 100%;
+      height: 52px;
+      line-height: 52px;
+      font-family: PingFangSC-Semibold;
+      font-size: 16px;
+      color: #FFFFFF;
+      text-align: center;
+      background: #6ED9CA;
+      margin-top: 5px;
+    }
   }
 </style>
 <script>
-export default {
-  data () {
-    return {
-      jobId: 0
-    }
-  },
-  mounted () {
-    console.log(this.$router.params, this.$route.params.id)
-    this.jobId = this.$route.params.id
-  },
-  methods: {
-    back () {
-      this.$router.push('/')
+  import DetailHeader from './header.vue'
+
+  export default {
+    components: {
+      DetailHeader
+    },
+
+    data () {
+      return {
+        jobId: 0,
+        is_more: false
+      }
+    },
+    mounted () {
+      this.jobId = this.$route.params.id
+    },
+    methods: {
+      moreDetail () {
+        this.is_more = !this.is_more
+      },
+      back () {
+        this.$router.push('/')
+      }
     }
   }
-}
 </script>
 
 
---
+++ pages/details/header.vue
@@ -0,0 +1,71 @@
+<!-- 详情页头部 -->
+<template>
+  <div class="header">
+    <div class="header-box">
+      <span><img class="i-back" @click="onBack()" src="//assets.qkcdn.com/images/3cf1683fe6243ff18c9dfa4666838194.png"></span>
+      <div class="title" v-text="title">兼聘兼职</div>
+      <span v-if="hasSearch"><img class="i-search" src="//assets.qkcdn.com/images/77afe7d6166f2cd816dd3dcce1651f49.png"></span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .header {
+    position: relative;
+    padding: 0 16px;
+    height: 44px;
+    background: #FFFFFF;
+    display: flex;
+    align-items: center;
+    justify-items: center;
+
+    .header-box {
+      position: relative;
+      border-bottom: solid 1px #eeeeee;
+      width: 100%;
+      height: 100%;
+    }
+
+    .i-back {
+      position: absolute;
+      width: 8px;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+    .title {
+      width: 100%;
+      font-family: PingFangSC-Regular;
+      font-size: 17px;
+      color: #444749;
+      letter-spacing: 0;
+      text-align: center;
+      line-height: 44px;
+    }
+    .i-search {
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      width: 20px;
+      right: 0;
+    }
+  }
+</style>
+
+<script>
+  export default {
+    props: {
+      title: String,
+      hasSearch: {
+        type: Boolean,
+        default: true
+      },
+      onBackButtonClick: Function
+    },
+
+    methods: {
+      onBack () {
+        if (this.onBackButtonClick) this.onBackButtonClick()
+      }
+    }
+  }
+</script>
