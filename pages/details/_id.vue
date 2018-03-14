e794b50506ce598b1be86af0e32ba4f1cc2dbd52
huangsenda <huang.senda@qianka.com>
2018-03-14 11:31:06 +0800

    模拟参数

--- pages/details/_id.vue
+++ pages/details/_id.vue
@@ -6,41 +6,41 @@
       :on-back-button-click="back"
     ></DetailHeader>
     <div class="detail-title">
-      <p class="title">苹果手机APP试玩在家随时可做</p>
-      <p class="salary"><span class="num">50</span>元/小时</p>
+      <p class="title" v-text="payload.title"></p>
+      <p class="salary"><span class="num" v-text="payload.currency"></span><span v-text="payload.currency_unit"></span></p>
       <div class="label">
-        <div class="label-item">日结</div>
-        <div class="label-item">9999人</div>
+        <div class="label-item" v-text="payload.jiexi_type"></div>
+        <div class="label-item" v-text="payload.jianpin_person"></div>
         <div class="clear"></div>
       </div>
-      <span><img class="hot" src="//assets.qkcdn.com/images/302d958266ec83b5f0762e3f443400d6.png"></span>
+      <span v-if="payload.tag"><img class="hot" src="//assets.qkcdn.com/images/302d958266ec83b5f0762e3f443400d6.png"></span>
     </div>
 
     <div class="detail-detail">
       <div class="title">职位描述</div>
-      <div class="description">资邸平台提供高净值客户针对优质抵押物进行投资的信息撮合服务，主打一线城市住宅别墅，提供一系列完善的解决方案，所有项目均通过国家房地产交易中心办理抵押登记，以此确保借款人房产抵押在出借人本人名下，保证本金利息权益，实现财富稳定增长。</div>
+      <div class="description" v-text="payload.jianpin_detail"></div>
       <div class="btn-more" @click="moreDetail"><span v-text="is_more?'收起': '展开'"></span><img class="btn-more-img" :src="is_more?'//assets.qkcdn.com/images/396f6c3c0fd033f6be32cc660001d007.png': '//assets.qkcdn.com/images/949066b0ae6761f707d74a6c45b0e014.png'"></div>
       <div class="clear"></div>
       <div class="income">
         <div class="title">薪资福利</div>
-        <div class="content">每天结算，50/小时</div>
+        <div class="content" v-text="payload.welfare"></div>
       </div>
     </div>
 
     <div class="detail-more" v-if="is_more">
       <div class="detail-time">
         <div class="title">工作时间</div>
-        <div class="content">不限时间地点，随时可做</div>
+        <div class="content" v-text="payload.work_time"></div>
       </div>
       <div class="detail-add">
         <div class="title">工作地点</div>
-        <div class="content">全国－不限</div>
+        <div class="content" v-text="payload.work_adress"></div>
       </div>
     </div>
 
     <div class="detail-more detail-co" v-if="is_more">
       <span class="name-title">公司名称</span>
-      <span class="name">方龙</span>
+      <span class="name" v-text="payload.company"></span>
     </div>
 
     <div class="btn-commit">我要报名</div>
@@ -243,7 +243,27 @@
     data () {
       return {
         jobId: 0,
-        is_more: false
+        is_more: false,
+        payload: {
+          'id': 1,
+          'title': '标题',
+          'city_name': '城市名',
+          'tag': '标签',
+          'jianpin_type': '招聘方式：1：长期招聘，2：指定时间',
+          'jianpin_starttime': '招聘开始时间',
+          'jianpin_endtime': '招聘结束时间',
+          'currency': '50',
+          'currency_unit': '元/小时',
+          'jiexi_type': '日结',
+          'jianpin_person': '9人',
+          'jianpin_detail': '职位描述',
+          'welfare': '每天结算，50/小时',
+          'work_time': '不限时间地点，随时可做',
+          'work_adress': '全国－不限',
+          'company': '方龙',
+          'jump_url': '跳转的url',
+          'jump_type': '跳转方式：1：URL，2：wechat'
+        }
       }
     },
     mounted () {
