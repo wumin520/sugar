547ae26d45bf82064caebb4829ca4815a90671e6
wumin <wu.min@qianka.com>
2018-05-08 09:39:31 +0800

    公众号修改

--- pages/contact.vue
+++ pages/contact.vue
@@ -21,7 +21,7 @@
             <i @click="isShowPopup = false" class="i-close"></i>
             <div class="logo-wrap"><img class="i-logo" src="//assets.qkcdn.com/images/a8cfe59a62060fe5a45fc387d80d6efb.png"></div>
             <div class="title">复制成功</div>
-            <div class="desc">您已成功复制微信号，请打开微信>通讯录>公众号>右上角添加>搜索“swapp321”并关注</div>
+            <div class="desc">您已成功复制微信号，请打开微信>通讯录>公众号>右上角添加>搜索“<span v-text="weChatName"></span>”并关注</div>
             <a style="display: none;" class="btn-wrap" href="weixin://"><button class="btn-primary">确定</button></a>
         </div>
     </div>
